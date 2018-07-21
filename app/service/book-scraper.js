355508d42480c828f3c58572ac745b4d85c95af3
Vũ Anh Dũng <dungva1505@gmail.com>
2018-07-21 12:43:38 +0700

    Add comment

--- app/service/book-scraper.js
+++ app/service/book-scraper.js
@@ -2,6 +2,11 @@ import scrapingCaller from '../lib/3rd-caller';
 import bookExtrator from '../lib/book-elm-extractor';
 import categoryExtractor from '../lib/category-elm-extractor';
 
+/**
+ * Validate parameter
+ * @param {*} offset Offset param got on request
+ * @returns {int} return pageNumber valid if OK, else throw Error
+ */
 const paramsValidator = (offset) => {
   const pageNumber = parseInt(offset, 10);
   if (!pageNumber) {
@@ -11,6 +16,12 @@ const paramsValidator = (offset) => {
   return pageNumber;
 };
 
+/**
+ * Scraper function
+ * @param {*} pageNumber PageNumber want to scraping
+ * @param {String} category Category path
+ * @param {String} init String or Boolean to mark request is init or not
+ */
 const scraper = async (pageNumber, category, init) => {
   const api = process.env.API || 'books.toscrape.com';
   let url;
