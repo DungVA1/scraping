5b6747f1734a9c768384a519d563ee70cd971e26
Vũ Anh Dũng <dungva1505@gmail.com>
2018-07-21 12:52:02 +0700

    ADd comment

--- app/lib/book-elm-extractor.js
+++ app/lib/book-elm-extractor.js
@@ -1,10 +1,15 @@
+
+/**
+ * Get value from pure HTML
+ * @param {DOM} $ HTML DOM
+ */
 const extractor = ($) => {
   const elms = $('section>div>ol');
   const responses = [];
   elms.find('li').each((i, elm) => {
     responses.push({
       bookName: $(elm).find('article>h3>a').html(),
-      category: 'Not Found',
+      category: 'Not Found', /* TODO: I can't find any element has category */
       image: $(elm).find('article>div[class=image_container]>a>img').attr('src'),
       rateStars: $(elm).find('article>p').attr('class').split(' ')[1],
       price: $(elm).find('article>div[class=product_price]>p[class=price_color]').html(),
